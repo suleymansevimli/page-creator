1918467b4d216115ab150eab4104ba0ba74c509b
Suleyman Sevimli <suleyman.sevimli1@gmail.com>
2019-04-07 02:34:51 +0300

    edit components and template design

--- js/main.js
+++ js/main.js
@@ -6,6 +6,7 @@ $(function() {
     $( "#headWrap" ).droppable({
       accept: "#head",
       drop: function( event, ui ) {
+        $(this).find('span').html('');
          $( this )
         .find( "h1" )
         .html( "Head Dropped !" );
@@ -14,6 +15,8 @@ $(function() {
     $( "#imageWrap" ).droppable({
       accept: "#image",
       drop: function( event, ui ) {
+        // $(this).html(ui.draggable.remove().html());
+        $(this).find('span').html('');
          $( this )
         .find( "div" )
         .html( "<img height=\"400\" width=\"400\" src=\"img/wood-home.jpg\"/>" )
@@ -22,6 +25,7 @@ $(function() {
     $( "#prgWrap" ).droppable({
       accept: "#paragraph",
       drop: function( event, ui ) {
+        $(this).find('span').html('');
          $( this )
         .find( "p" )
         .html( "Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum." )
